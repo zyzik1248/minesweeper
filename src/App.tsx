f54f28e2bb47fd274fc8de60f7aef856ffa516d6
zuzanna dudzinska <zuzanna.dudzinska@mayeryn.com>
2020-12-19 23:29:34 +0100

    Fix router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 
 import Minesweeper from './pages/Minesweeper'
 import MainPage from './pages/MainPage'
@@ -11,13 +11,13 @@ const App = () => {
   return (
     <div>
       <Settings />
-      <BrowserRouter>
+      <HashRouter>
         <Switch>
           <Route exact path='/' component={MainPage} />
           <Route exact path='/game' component={Minesweeper} />
           <Route exact path='/score' component={Score} />
         </Switch>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
