3e54266f76966248ae961267a30f33f473f6e969
zuzanna dudzinska <zuzanna.dudzinska@mayeryn.com>
2020-12-21 22:29:54 +0100

    fixup! Fix responsiveness

--- src/minesweeper/board/Board.css.tsx
+++ src/minesweeper/board/Board.css.tsx
@@ -13,11 +13,13 @@ export const BoardStyled = styled.div<props>`
   ${(props: props) => props.windowHeight * (85 / props.height) / 100 * props.width < props.windowWidth ?
     `grid-template-rows: repeat(${props.height}, ${props.windowHeight * (85 / props.height) / 100 < 30 ? `30px` : `1fr`});
     grid-template-columns: repeat(${props.width}, ${props.windowHeight * (85 / props.height) / 100 < 30 ? `30px` : `${85 / props.height}vh`});
-    height: 100%;` :
+    ` :
     `grid-template-columns: repeat(${props.width}, ${props.windowWidth * (95 / props.width) / 100 < 30 ? `30px` : `1fr`});
     grid-template-rows: repeat(${props.height}, ${props.windowWidth * (95 / props.width) / 100 < 30 ? `30px` : `${95 / props.width}vw`});
-    width: 100%;`
+    `
   }
+  width: 100%;
+  height: 100%;
   grid-gap: 0px;
   letter-spacing: 0px;
   word-spacing: 0px;
@@ -26,9 +28,8 @@ export const BoardStyled = styled.div<props>`
 export const Wrapper = styled.div`
   height: 85vh;
   width: 95vw;
-  padding: 10px;
   display: flex;
-  margin: 20px auto;
+  margin: 25px auto;
   justify-content: center;
   align-items: center;
   overflow-y: auto;
