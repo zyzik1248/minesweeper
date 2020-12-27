f94aef8ca36e3d9fd1df0793b8f91c35dbb0d1d8
zuzanna dudzinska <zuzanna.dudzinska@mayeryn.com>
2020-12-27 11:46:49 +0100

    Fix score

--- src/pages/Score.tsx
+++ src/pages/Score.tsx
@@ -40,80 +40,78 @@ const Score = () => {
   }
 
   useEffect(() => {
-    setTimeout(() => {
-      if (winners.winners.length === 0) {
-        readData().then((d) => {
-          const data: Winner[] = d as Winner[];
+    if (winners.winners.length === 0) {
+      readData().then((d) => {
+        const data: Winner[] = d as Winner[];
 
-          for (let i = 0; i < 3; i++) {
+        for (let i = 0; i < 3; i++) {
 
-            const w: Winner[] = data.filter((value: any) => {
-              return value.level === 2 - i;
-            }).sort((a: Winner, b: Winner) => sortByTime(a, b))
+          const w: Winner[] = data.filter((value: any) => {
+            return value.level === 2 - i;
+          }).sort((a: Winner, b: Winner) => sortByTime(a, b))
 
-            if (w.length > 0) {
-              winners.winners.push(w);
-            }
+          if (w.length > 0) {
+            winners.winners.push(w);
           }
+        }
 
-          let wCustom: Winner[] = data.filter((value: Winner) => {
-            return value.level === 3;
-          }).sort((a: Winner, b: Winner) => sortCustom(a, b))
+        let wCustom: Winner[] = data.filter((value: Winner) => {
+          return value.level === 3;
+        }).sort((a: Winner, b: Winner) => sortCustom(a, b))
 
-          while (wCustom.length > 0) {
-            winners.winners.push(wCustom.filter((value: Winner) => {
-              return value.height === wCustom[0].height && value.width === wCustom[0].width
-                && value.mines === wCustom[0].mines
-            }).sort((a: Winner, b: Winner) => sortByTime(a, b)))
+        while (wCustom.length > 0) {
+          winners.winners.push(wCustom.filter((value: Winner) => {
+            return value.height === wCustom[0].height && value.width === wCustom[0].width
+              && value.mines === wCustom[0].mines
+          }).sort((a: Winner, b: Winner) => sortByTime(a, b)))
 
-            wCustom.splice(0, wCustom.length);
-            const arr = wCustom.filter((value: Winner) => {
-              return !(value.height === wCustom[0].height && value.width === wCustom[0].width
-                && value.mines === wCustom[0].mines)
-            })
+          const arr = wCustom.filter((value: Winner) => {
+            return !(value.height === wCustom[0].height && value.width === wCustom[0].width
+              && value.mines === wCustom[0].mines)
+          })
 
-            for(let i=0; i<arr.length; i++){
-              wCustom[i] = arr[i]
-            }
+          wCustom.splice(0, wCustom.length);
+          for (let i = 0; i < arr.length; i++) {
+            wCustom.push(arr[i])
           }
+        }
 
-          setWinners({ winners: winners.winners })
-          setIsEmpty(data.length === 0)
-          setIsLoading(false)
-        })
-      }
-    }, 1000);
-  }, [winners]);
+        setWinners({ winners: winners.winners })
+        setIsEmpty(data.length === 0)
+        setIsLoading(false)
+      })
+    }
+}, [winners]);
 
-  return (
-    <Wrapper>
-      <Loading invisible={!isLoading} />
-      <h1>Score</h1>
-      {
-        isEmpty &&
-        <div>
-          there are no any winners
+return (
+  <Wrapper>
+    <Loading invisible={!isLoading} />
+    <h1>Score</h1>
+    {
+      isEmpty &&
+      <div>
+        there are no any winners
         </div>
-      }
-      {winners.winners.map((row) => (
-        <>
-          <TableTitle
-            level={row[0].level}
-            width={row[0].width}
-            height={row[0].height}
-            mines={row[0].mines}
+    }
+    {winners.winners.map((row) => (
+      <>
+        <TableTitle
+          level={row[0].level}
+          width={row[0].width}
+          height={row[0].height}
+          mines={row[0].mines}
+        />
+        {row.map((el, index2) => (
+          <SingleScore
+            id={index2 + 1}
+            name={el.name}
+            time={el.time}
           />
-          {row.map((el, index2) => (
-            <SingleScore
-              id={index2 + 1}
-              name={el.name}
-              time={el.time}
-            />
-          ))}
-        </>
-      ))}
-    </Wrapper>
-  )
+        ))}
+      </>
+    ))}
+  </Wrapper>
+)
 }
 
 export default Score
